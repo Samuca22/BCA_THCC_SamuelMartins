d758e6526a2dba0a3bfdaf92a590f8a4d1d59919
Samuca22 <47739958+Samuca22@users.noreply.github.com>
2026-03-09 22:49:55 +0000

    Update users.spec.js with PUT and DELETE tests

--- src/api/tests/users.spec.js
+++ src/api/tests/users.spec.js
@@ -13,40 +13,42 @@ test.describe('GET - User API - Manual structure validations', () => {
                 'x-api-key': process.env.API_KEY
             }
         });
-    
-        expect(response.ok()).toBeTruthy();
+
         expect(response.status()).toBe(200);
-    
-        const respondeBody = await response.json();
+        expect(response.ok()).toBeTruthy();
+
+        const responseBody = await response.json();
         // Assert pagination properties data type
-        expect(respondeBody.page).toEqual(expect.any(Number));
-        expect(respondeBody.per_page).toEqual(expect.any(Number));
-        expect(respondeBody.total).toEqual(expect.any(Number));
-        expect(respondeBody.total_pages).toEqual(expect.any(Number));
-        expect(respondeBody.data).toEqual(expect.any(Array));
+        expect(responseBody.page).toEqual(expect.any(Number));
+        expect(responseBody.per_page).toEqual(expect.any(Number));
+        expect(responseBody.total).toEqual(expect.any(Number));
+        expect(responseBody.total_pages).toEqual(expect.any(Number));
+        expect(responseBody.data).toEqual(expect.any(Array));
         // Assert data (users) array is not empty
-        expect(respondeBody.data.length).toBeGreaterThan(0);
+        expect(responseBody.data.length).toBeGreaterThan(0);
     });
 
     //TC02 - GET user by id
     test('Get - User by ID - Manual structure validations', async ({ request }) => {
-        const response = await request.get('/api/users/2', {
+        const id = 2;
+        const response = await request.get(`/api/users/${id}`, {
             headers: {
                 'Content-Type': 'application/json',
                 'x-api-key': process.env.API_KEY
             }
         });
-        expect(response.ok()).toBeTruthy();
-        expect(response.status()).toBe(200);
 
-        const respondeBody = await response.json();
+        expect(response.status()).toBe(200);
+        expect(response.ok()).toBeTruthy();
+
+        const responseBody = await response.json();
 
         // Assert data (user) property to be an object
-        expect(respondeBody.data).toEqual(expect.any(Object));
-        expect(respondeBody.data.id).toEqual(expect.any(Number));
-        expect(respondeBody.data.email).toEqual(expect.any(String));
-        expect(respondeBody.data.first_name).toEqual(expect.any(String));
-        expect(respondeBody.data.last_name).toEqual(expect.any(String));
+        expect(responseBody.data).toEqual(expect.any(Object));
+        expect(responseBody.data.id).toEqual(expect.any(Number));
+        expect(responseBody.data.email).toEqual(expect.any(String));
+        expect(responseBody.data.first_name).toEqual(expect.any(String));
+        expect(responseBody.data.last_name).toEqual(expect.any(String));
     });
 });
 
@@ -54,69 +56,125 @@ test.describe('GET - User API - Manual structure validations', () => {
 test.describe('GET - Users List - With schema validation', () => {
     //TC01 - GET users list (schema validation)
     test('Get users list', async ({ request }) => {
-    const response = await request.get('/api/users?page=2', {
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': process.env.API_KEY
-        }
-    });
+        const response = await request.get('/api/users?page=2', {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': process.env.API_KEY
+            }
+        });
 
-    expect(response.ok()).toBeTruthy();
-    expect(response.status()).toBe(200);
+        expect(response.status()).toBe(200);
+        expect(response.ok()).toBeTruthy();
 
-    const respondeBody = await response.json();
-    expect(validateSchema(usersListSchema, respondeBody)).toBeTruthy();
+        const responseBody = await response.json();
+        expect(validateSchema(usersListSchema, responseBody)).toBeTruthy();
     });
 
     //TC02 - GET user by id (schema validation)
     test('Get user by id', async ({ request }) => {
-    const response = await request.get('/api/users/2', {
-        headers: {
-            'Content-Type': 'application/json',
-            'x-api-key': process.env.API_KEY
-        }
-    });
-    expect(response.ok()).toBeTruthy();
-    expect(response.status()).toBe(200);
-    const respondeBody = await response.json();
-    // Call utils function for schema validation
-    const validationSchemaResult = validateSchema(userSchema, respondeBody);
-    // Expect true from validation and null from error message (if not throw error)
-    expect(
-        validationSchemaResult.isValid, 
-        validationSchemaResult.errorMessage)
-        .toBe(true, null);
-    });
+        const id = 2;
+        const response = await request.get(`/api/users/${id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': process.env.API_KEY
+            }
+        });
 
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        // Call utils function for schema validation
+        const validationSchemaResult = validateSchema(userSchema, responseBody);
+        // Expect true from validation and null from error message (if not throw error)
+        expect(
+            validationSchemaResult.isValid,
+            validationSchemaResult.errorMessage
+        ).toBe(true, null);
+    });
 });
 
 // POST
 test.describe('POST - Create user API test', () => {
-
     //TC01 - POST create user
     test('Create user - Manual structure validations', async ({ request }) => {
+        const data = {
+            name: 'Samuel Martins',
+            job: 'Automation Engineer'
+        };
+
         const response = await request.post('/api/users', {
             headers: {
                 'Content-Type': 'application/json',
                 'x-api-key': process.env.API_KEY
             },
-            data: {
-                name: 'Samuel Martins',
-                job: 'Automation Engineer'
-            }
+            data
         });
 
         expect(response.status()).toBe(201);
         expect(response.statusText()).toBe('Created');
 
-        const respondeBody = await response.json();
+        const responseBody = await response.json();
         // Assert passed data in the request body
-        expect(respondeBody.name).toEqual('Samuel Martins');
-        expect(respondeBody.job).toEqual('Automation Engineer');
+        expect(responseBody.name).toEqual(data.name);
+        expect(responseBody.job).toEqual(data.job);
         // Assert extra data: generated id and timestamp
-        expect(respondeBody.id).toEqual(expect.any(String))
-        
-        const createdAt = new Date(respondeBody.createdAt);
-        expect(createdAt).toEqual(expect.any(Date));
+        expect(responseBody.id).toEqual(expect.any(String))
+
+        const createdAtProperty = new Date(responseBody.createdAt);
+        expect(createdAtProperty).toEqual(expect.any(Date));
+    });
+});
+
+// PUT
+test.describe('PUT - Update user API test', () => {
+    //TC01 - PUT update user
+    test('Update user - Manual structure validations', async ({ request }) => {
+        const id = 2;
+        const data = {
+            name: 'Samuel Martins Updated',
+            job: 'Automation Engineer Updated to Senior'
+        };
+
+        const response = await request.put(`/api/users/${id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': process.env.API_KEY
+            },
+            data
+        });
+
+        expect(response.status()).toBe(200);
+        expect(response.ok()).toBeTruthy();
+
+        const responseBody = await response.json();
+        // Assert passed data in the request body
+        expect(responseBody.name).toEqual(data.name);
+        expect(responseBody.job).toEqual(data.job);
+
+        // Assert extra data: generated updatedAt property
+        const updatedAtProperty = new Date(responseBody.updatedAt);
+        expect(updatedAtProperty).toEqual(expect.any(Date));
+    });
+});
+
+
+// DELETE
+test.describe('DELETE - Delete user API test', () => {
+    //TC01 - DELETE delete user
+    test('Delete user with id- Manual structure validations', async ({ request }) => {
+        const id = 2;
+        const response = await request.delete(`/api/users/${id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': process.env.API_KEY
+            }
+        });
+
+        expect(response.status()).toBe(204);
+        expect(response.statusText()).toBe('No Content');
+
+        // Assert empty response body by text
+        const responseBody = await response.text();
+        expect(responseBody).toBe('');
     });
 });
