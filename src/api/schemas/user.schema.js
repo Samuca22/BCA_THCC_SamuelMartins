abb3a5c959315bcc4e261ed3d00944221815e39d
Samuca22 <47739958+Samuca22@users.noreply.github.com>
2026-03-09 08:27:53 +0000

    Create userSchema.js

---
+++ src/api/schemas/user.schema.js
@@ -0,0 +1,45 @@
+export const userSchema = {
+    type: 'object',
+    properties: {
+        data: {
+            id: {type: 'number'},
+            email: {type: 'string', format: 'email'},
+            first_name: {type: 'string'},
+            last_name: {type: 'string'},
+            avatar: {type: 'string', format: 'uri'},
+        },
+        required: ['id', 'email', 'first_name', 'last_name', 'avatar'],
+        additionalProperties: false,
+        support: {
+            url: {type: 'string', format: 'uri'},
+            text: {type: 'string'},
+        },
+        required: ['url', 'text'],
+        additionalProperties: false,
+        _meta: {
+            type: 'object',
+            properties: {
+                powered_by: {type: 'string'},
+                docs_url: {type: 'string', format: 'uri'},
+                upgrade_url: {type: 'string', format: 'uri'},
+                example_url: {type: 'string', format: 'uri'},
+                variant: {type: 'string'},
+                message: {type: 'string'},
+                cta: {
+                    type: 'object',
+                    properties: {
+                    label: {type: 'string'},
+                    url: {type: 'string', format: 'uri'},
+                    },
+                required: ['label', 'url'],
+                additionalProperties: false,
+            },
+            context: {type: 'string'},
+            required: ['powered_by', 'docs_url', 'upgrade_url', 'example_url', 'variant', 'message', 'cta', 'context'],
+                additionalProperties: false,
+            },
+        },
+        required: ['data', 'support', '_meta'],
+        additionalProperties: false,
+    },
+};
